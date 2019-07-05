d3097ce3475736dbb73c17e3939dc6cdcfd2c472
Richer Archambault <richer.arc@gmail.com>
2019-07-05 11:04:31 -0400

    U2f object added

--- slauth/src/androidTest/java/net/devolutions/slauth/ExampleInstrumentedTest.java
+++ slauth/src/androidTest/java/net/devolutions/slauth/ExampleInstrumentedTest.java
@@ -7,6 +7,9 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
 import static org.junit.Assert.*;
 
 /**
@@ -38,5 +41,31 @@ public class ExampleInstrumentedTest {
         String code2 = totp.genWith(31);
 
         assertEquals(code1, code2);
+    }
+
+    @Test
+    public void u2fTest() {
+        try {
+            byte[] att_cert = Base64.getDecoder().decode("MIICODCCAd6gAwIBAgIJAKsa9WC9HvEuMAoGCCqGSM49BAMCMFoxDzANBgNVBAMMBlNsYXV0aDELMAkGA1UEBhMCQ0ExDzANBgNVBAgMBlF1ZWJlYzETMBEGA1UEBwwKTGF2YWx0cm91ZTEUMBIGA1UECgwLRGV2b2x1dGlvbnMwHhcNMTkwNzAyMTgwMTUyWhcNMzEwNjI5MTgwMTUyWjBaMQ8wDQYDVQQDDAZTbGF1dGgxCzAJBgNVBAYTAkNBMQ8wDQYDVQQIDAZRdWViZWMxEzARBgNVBAcMCkxhdmFsdHJvdWUxFDASBgNVBAoMC0Rldm9sdXRpb25zMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE15PAnpUUIzbgKxD6RFuNMjjl/cD06vKRBtl0X/CiNzc3igTh1qcc00QICgAQUxdvHSn+DaSRki/kI9OJ8lkPGqOBjDCBiTAdBgNVHQ4EFgQU7iZ4JceUHOuWoMymFGm+ZBUmwwgwHwYDVR0jBBgwFoAU7iZ4JceUHOuWoMymFGm+ZBUmwwgwDgYDVR0PAQH/BAQDAgWgMCAGA1UdJQEB/wQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAVBgNVHREEDjAMggpzbGF1dGgub3JnMAoGCCqGSM49BAMCA0gAMEUCIEdjPFNsund4FXs/1HpK4AXWQ0asfY6ERhNlg29VGS6pAiEAx8f2lrlVV1tASWbC/edTgH9JsCbANuXW/9FZcWHGl2E=".getBytes("UTF-8"));
+            byte[] att_key = Base64.getDecoder().decode("MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgzgUSoDttmryF0C+ck4GppKwssha7ngah0dfezfTBzDOhRANCAATXk8CelRQjNuArEPpEW40yOOX9wPTq8pEG2XRf8KI3NzeKBOHWpxzTRAgKABBTF28dKf4NpJGSL+Qj04nyWQ8a".getBytes("UTF-8"));
+
+            String json = "{\"appId\":\"https://login.devolutions.com/\",\"registerRequests\":[{\"challenge\":\"UzAxNE0yMTBWM1JDYzA1a1JqWndRUT09\",\"version\":\"U2F_V2\"}],\"registeredKeys\":[],\"requestId\":1,\"timeoutSeconds\":300,\"type\":\"u2f_register_request\"}";
+
+            WebRequest web_r = new WebRequest(json);
+
+            String origin = web_r.getOrigin();
+
+            WebResponse rsp = web_r.register(origin, att_cert, att_key);
+
+            SigningKey key = rsp.getSigningKey();
+
+            System.out.println(key.getKeyHandle());
+            System.out.println(key.toString());
+            System.out.println(rsp.toJson());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (InvalidResponseTypeException e) {
+            e.printStackTrace();
+        }
     }
 }
--- slauth/src/main/java/net/devolutions/slauth/JNA.java
+++ slauth/src/main/java/net/devolutions/slauth/JNA.java
@@ -36,4 +36,36 @@ public interface JNA extends Library {
     Boolean totp_validate_current(Pointer totp, String code);
 
     Boolean totp_verify(Pointer totp, String code);
+
+    void client_web_response_free(Pointer rsp);
+
+    Pointer client_web_response_signing_key(Pointer rsp);
+
+    String client_web_response_to_json(Pointer rsp);
+
+    void signing_key_free(Pointer s);
+
+    Pointer signing_key_from_string(String s);
+
+    String signing_key_to_string(Pointer s);
+
+    String signing_key_get_key_handle(Pointer s);
+
+    void web_request_free(Pointer req);
+
+    Pointer web_request_from_json(String req);
+
+    Boolean web_request_is_register(Pointer req);
+
+    Boolean web_request_is_sign(Pointer req);
+
+    String web_request_key_handle(Pointer req, String origin);
+
+    String web_request_origin(Pointer req);
+
+    Pointer web_request_register(Pointer req, String origin, byte[] attestation_cert, long attestation_cert_len, byte[] attestation_key, long attestation_key_len);
+
+    Pointer web_request_sign(Pointer req, Pointer signing_key, String origin, long counter, Boolean user_presence);
+
+    long web_request_timeout(Pointer req);
 }
